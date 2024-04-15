e5f29f8bbe35b15950ea2645baf290e09f9f05d7
Torycd <vicidundun@gmail.com>
2024-04-15 22:51:50 +0100

    hmm

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -1,10 +1,34 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 let globalState = {};
 let listeners = [];
-let actions ={};
+let actions = {};
 
-const useStore = () => {
-    useState(globalState)
-}
+export const useStore = () => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier) => {
+    const newState = actions[actionIdentifier](globalState);
+    const globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+  useEffect(() => {
+    listeners.push(setState);
+
+    return () => {
+      listeners = listeners.fill((l) => !setState);
+    };
+  }, [setState]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, intialState) => {
+  if (intialState) {
+    globalState = { ...globalState, intialState };
+  }
+  actions = { ...actions, ...userActions };
+};
